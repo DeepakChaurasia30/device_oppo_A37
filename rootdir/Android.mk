a6dc9b7ea9665c9b84290a7ed443db4872f80e2a
AndroNoman <chaurasiaanita391@gmail.com>
2021-03-26 12:42:52 +0530

    bluetooth:clean-up

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,14 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 # QCOM config scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mmi.bt.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := etc/init.mmi.bt.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.mem.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
--- rootdir/etc/init.mmi.bt.sh
+++
@@ -1,5 +0,0 @@
-#!/vendor/bin/sh
-
-btaddr=`getprop ro.boot.btmacaddr | sed 's/../&:/g;s/:$//'`
-
-setprop persist.service.bdroid.bdaddr $btaddr
