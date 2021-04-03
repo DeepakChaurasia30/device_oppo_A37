0d0eace3e44cd6a0a2407e7c9d27f418713ad117
AndroNoman <chaurasiaanita391@gmail.com>
2021-04-03 16:06:24 +0530

    Drop Video_call prop It enable video calling feature but we don't have support so better to disable it.

--- device.mk
+++ device.mk
@@ -467,7 +467,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
      persist.dbg.ims_volte_enable=1 \
      persist.dbg.volte_avail_ovr=1 \
-     persist.dbg.vt_avail_ovr=1 \
      persist.dbg.wfc_avail_ovr=1
     
 # Seccomp
