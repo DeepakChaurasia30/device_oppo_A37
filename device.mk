13febc01bad618ff4ecc443c7762dc81c7bb09b4
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-06-25 16:39:26 +0530

    Migrate to camera.device@3.2-imp.

--- device.mk
+++ device.mk
@@ -196,8 +196,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     libshim_camera \
     libcamera_shim \
     camera.msm8916 \
