ed9fa2089817ed095f3e88f15c9ea170dfb0a024
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-06-27 01:05:33 +0530

    Revert "Migrate to camera.device@3.2-imp."
    This reverts commit 13febc01bad618ff4ecc443c7762dc81c7bb09b4.

--- device.mk
+++ device.mk
@@ -196,7 +196,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     libshim_camera \
     libcamera_shim \
     camera.msm8916 \
