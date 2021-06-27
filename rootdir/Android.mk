47cf707454b873214f5e49cbeb4a970b17f110f1
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-06-27 15:42:24 +0530

    Drop fde_ext4_resize.sh.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,13 +25,6 @@ LOCAL_SRC_FILES    := etc/init.boot_boost.sh
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fde_ext4_resize.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := etc/fde_ext4_resize.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
 
 # Init scripts
 include $(CLEAR_VARS)
