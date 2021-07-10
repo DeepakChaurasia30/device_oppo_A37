d3ee224c62f545c7c1d658f1fef752ee5bce69af
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-07-10 18:23:01 +0530

    Switch to qti power hal -2

--- device.mk
+++ device.mk
@@ -344,8 +344,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.max_starting_bg=8
