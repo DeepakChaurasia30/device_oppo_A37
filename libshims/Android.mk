896cb9b67cd9728ed2d50abcef27114ed7c4fede
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-07-16 16:31:15 +0530

    Rvert of commit 4befc1afa85ac80c379c8ab505aef20c468cc3c8

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -45,14 +45,3 @@ LOCAL_MODULE := libcamera_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-    ui/GraphicBuffer.cpp 
-
-LOCAL_SHARED_LIBRARIES := libui
-LOCAL_MODULE := libui_shim
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
